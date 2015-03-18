4a1e21ca519e8e5e3c47b62f49e13308d6c412af
Eugene Narovlyansky <cybdoom@gmail.com>
2015-03-18 18:09:39 +0200

    [Seeds] Improved to handle multiple envs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-mask = File.join File.dirname(__FILE__), 'seeds', '*.rb'
+env = Rails.env || 'development'
+mask = File.join File.dirname(__FILE__), 'seeds', env, '*.rb'
 Dir[mask].each { |file_name| require file_name }
