76882698aaca154af8ab22b2ccf5dbe6de7a66bd
Eugene Narovlyansky <cybdoom@gmail.com>
2015-03-19 17:05:00 +0200

    [Waypoints] Added mongoid model for waypoints

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,5 +1,19 @@
 class Waypoint
   include Mongoid::Document
 
+  # CUSTOM FIELDS
+  # field :idx, type: Integer # ?
+  field :title, type: String
+  field :description, type: String
+  field :street, type: String
+  field :postal_code, type: String
+  field :city, type: String
+  field :country, type: String
+  field :state, type: String
+  field :iso_code, type: String
+  # # # # # # # #
 
+  # RELATIONS
+  embeds_one :point
+  # # # # # #
 end
